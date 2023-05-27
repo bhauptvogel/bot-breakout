9ed24703c1c65c4c83c7e5b8b88130c2ae38395d
stephanie <sd9721.sd@gmail.com>
2023-05-27 19:43:11 +0200

    update actions for motive

--- chatbot/actions/actions.py
+++ chatbot/actions/actions.py
@@ -5,27 +5,6 @@
 # https://rasa.com/docs/rasa/custom-actions
 
 
-# This is a simple example for a custom action which utters "Hello World!"
-
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
-
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker, FormValidationAction
@@ -39,43 +18,137 @@ class ValidateSimpleAgeForm(FormValidationAction):
      def name(self) -> Text:
           return "validate_simple_age_form"
 
-     def validate_age(
-               self, 
-               slot_value: Any, 
-               dispacher: CollectingDispatcher, 
-               tracker: Tracker, 
-               domain: DomainDict,
-     ) -> Dict[Text, Any]: 
-          """Validate `age` value."""
-
-          if int(slot_value) != 25:
-               random_number = random.randint(0, 2)
-               if random_number == 0:
-                    dispacher.utter_message(text=f"No that's wrong. I'm not {slot_value}")
-               elif random_number == 1:
-                    dispacher.utter_message(text=f"No, {slot_value} is not my age")
-               else: 
-                    dispacher.utter_message(text=f"Nope I'm not {slot_value}")
-          else:
-               dispacher.utter_message(text=f"Yes! I'm {slot_value} years old")
-               return {"age": slot_value}
-
-
-class ActionLocationSearch(Action):
-
+class GiveHint(Action):
     def name(self) -> Text:
-        return "action_restaurant_search"
+        return "action_hint"
 
     def run(self, dispatcher: CollectingDispatcher,
-          tracker: Tracker,
-          domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-          location = next(tracker.get_latest_entity_values("location"), None)
-          place = next(tracker.get_latest_entity_values("place"), None)
+        dispatcher.utter_message(text='TODO: Give hint (answer to last question)')
 
-          #dispatcher.utter_message(text=f"Perfect let's go to a {place} in {location}! I'll send you the details.")
-#          query = str(place)+" in "+str(location)
-#          search = googlesearch.search(query)
-#          dispatcher.utter_message(f"Query: {query}")
-            
-#          return []
+        return []
+
+class SituationOverview(Action):
+    def name(self) -> Text:
+        return "action_overview_of_the_state"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text='TODO: Give situation overview')
+
+        return []
+
+class SceneInvestigation(Action):
+    def name(self) -> Text:
+        return "action_scene_investigation"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        entities = tracker.latest_message['entities']
+        objects = [e['value'] for e in entities if e['entity'] == 'object']
+
+        dispatcher.utter_message(text=('TODO: Investigate ' + ', '.join(objects) + '...'))
+
+        return []
+
+class CharacterInvestigation(Action):
+    def name(self) -> Text:
+        return "action_character_investigation"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        entities = tracker.latest_message['entities']
+        characters = [e['value'] for e in entities if e['entity'] == 'person']
+        if tracker.get_slot('data') is None or tracker.get_slot('data') == 'Null':
+            data = {}
+        else:
+            data = tracker.get_slot('data')
+
+        story_characters = {"Maria": {1: "Maria is the Ex-girlfriend of my co-worker Kira. Do you want to know more about Kira or should we investigate the cabin a bit more?", 2: "Information 2 about Maria"},
+                            "Kira" : {1 : "Information 1 about Kira", 2: "Information 2 about Kira"},
+                            "Patrick": {1 : "Information 1 about Kira", 2: "Information 2 about Kira"},
+                            "Victor": {1 : "Information 1 about Victor", 2: "Information 2 about Victor"},
+                            "Anna": {1 : "Information 1 about Anna", 2: "Information 2 about Anna"}}
+
+        for story_character in story_characters:
+            if story_character in characters:
+                if "times_asked_about_" + story_character not in data:
+                    data["times_asked_about_" + story_character] = 1
+                    dispatcher.utter_message(text=(story_characters[story_character][1]))
+                else:
+                    data["times_asked_about_" + story_character] += 1
+                    if data["times_asked_about_" + story_character] == 2:
+                        dispatcher.utter_message(text=(story_characters[story_character][2]))
+                    else:
+                        dispatcher.utter_message(text=("You already asked me about " + story_character + " but sure, her you go: " + story_characters[story_character][2]))
+
+
+
+
+        informations = [e['value'] for e in entities if e['entity'] == 'information']
+        dispatcher.utter_message(text=('TODO: Investigate characters: ' + ', '.join(characters) + ' / informations: ' + ', '.join(informations) + '...'))
+
+        return [SlotSet("data", data)]
+
+
+class CharacterMotive(Action):
+    def name(self) -> Text:
+        return "action_tell_motive"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        entities = tracker.latest_message['entities']
+        suspects = [e['value'] for e in entites if e['group'] == 'suspect' and e['entity'] == 'person']
+        victim = [e['value'] for e in entites if e['group'] == 'victim' and e['entity'] == 'person']
+        characters = [e['value'] for e in entities if e['entity'] == 'person']
+
+        if tracker.get_slot('data') is None or tracker.get_slot('data') == 'Null':
+            data = {}
+        else:
+            data = tracker.get_slot('data')
+
+        story_characters = {"Kira" : {1 : "Motive of Kira", 2: "Second answer if asked again"},
+                            "Patrick": {1 : "Motive of Patrick", 2: "Second answer if asked again"},
+                            "Victor": {1 : "Motive of Victor", 2: "Second answer if asked again"},
+                            "Anna": {1 : "Motive of Anna", 2: "Second answer if asked again"}}
+
+        for story_character, motive in story_characters.items():
+            if story_character in characters:
+                if "times_asked_about_" + story_character not in data:
+                    data["times_asked_about_" + story_character] = 1
+                    dispatcher.utter_message(text=(motive[1]))
+                else:
+                    data["times_asked_about_" + story_character] += 1
+                    if data["times_asked_about_" + story_character] == 2:
+                        dispatcher.utter_message(text=(motive[2]))
+                    else:
+                        dispatcher.utter_message(text=("You already asked me about " + story_character + " but sure, here you go: " + motive[2]))
+
+        dispatcher.utter_message(text='TODO: Tell motive of ' + ', '.join(suspects) + ' for killing ' + ', '.join(victim) + '...')
+
+        return []
+
+
+class AccessToRollerCoaster(Action):
+    def name(self) -> Text:
+        return "action_access_to_roller_coaster"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        entities = tracker.latest_message['entities']
+        characters = [e['value'] for e in entities if e['entity'] == 'person']
+        dispatcher.utter_message(text='TODO: Tell access to roller coaster of ' + ', '.join(characters) + '...')
+
+        return []
