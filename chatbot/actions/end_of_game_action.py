826786cb9793d83e5bdb8a1d8d7329f08542b141
bhauptvogel <b.hauptvogel@campus.tu-berlin.de>
2023-05-31 23:30:57 +0200

    end of game...

--- chatbot/actions/end_of_game_action.py
+++ chatbot/actions/end_of_game_action.py
@@ -65,7 +65,8 @@ class UserGuessesMurderer(Action):
                     text="Alright, thanks for clearing my mind. I'm not a hundred percent sure but I trust you. Let's go now and look for the police to tell them... We have to be right here! \n\n [Police Officer] Hey, I'm police officer Kramer. I heard about the dead body you found. Is there anything you want to tell me? Who do you suspect?"
                 )
                 data["user_wants_to_commit"] = True
-            else:
+            elif data["user_wants_to_commit"] == True:
+                data["user_wants_to_commit"] = False
                 if person[0] == "Patrick":
                     dispatcher.utter_message(
                         "Haha seams like you already did my Job! We will check all the details and talk to you after we are done. Please leave some of your informations to my colleage. Possible, that we will be in touch sone. But for now you can leave.\n"
@@ -74,6 +75,8 @@ class UserGuessesMurderer(Action):
                         "After a hot investigation the police found multiple hints to claim Patrick for corruption. Your perfect Hint was very helpful and leaded to Patrick being in jail very quick. Police work is your ambition. Great job. Your Date is impressed too and ask for another Date. Maybe you will solve a theft this time!"
                     )
                     dispatcher.utter_message("You won the game! Congratulations!")
+                    # end game
+
                 else:
                     dispatcher.utter_message(
                         f"So you already did my job. We talked with {person[0]} before we arrived. They have an alibi... maybe I should talk to you two bit more. Maybe on the Police Station. Hendrick! Handcuff these two, they are suspiscious.\n"
@@ -84,6 +87,10 @@ class UserGuessesMurderer(Action):
                     dispatcher.utter_message(
                         "You lost the game! Better luck next time!"
                     )
+            else:
+                dispatcher.utter_message(
+                    text="Game is over. You can't do anything anymore."
+                )
         else:
             dispatcher.utter_message(
                 text="We can’t leave before the police arrives in a few minutes! We need to find hints together, so they don’t think we two did it. Let's investigate..."
