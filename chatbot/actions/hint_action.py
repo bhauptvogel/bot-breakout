b9e9ca8772ac7e071fbeae42c07d8b90162ae872
bhauptvogel <b.hauptvogel@campus.tu-berlin.de>
2023-06-05 21:11:51 +0200

    naming variables

--- chatbot/actions/hint_action.py
+++ chatbot/actions/hint_action.py
@@ -10,62 +10,62 @@ HINTS = [
     # not talked about coworkers
     {
         "text": "Maybe we can talk about my coworkers. I have a hunch they play an important role in all of this.",
-        "not_in_game_state": ["character_information"],
+        "disallowed_story_states": ["character_information"],
     },
     # scene not investigated
     {
         "text": "Let me look at the surroundings, should I? Investigating the scene might help us.",
-        "not_in_game_state": ["scene_investigation"],
+        "disallowed_story_states": ["scene_investigation"],
     },
     # not talked about Maria (victim)
     {
         "text": "I think we should talk about Maria. She was the victim after all.",
-        "not_in_game_state": ["character_information/Maria"],
+        "disallowed_story_states": ["character_information/Maria"],
     },
     # not talked about Anna
     {
         "text": "Maybe you should know more about Anna. There is something important you need to know!",
-        "not_in_game_state": ["character_information/Anna"],
+        "disallowed_story_states": ["character_information/Anna"],
     },
     # cabin not investigated
     {
         "text": "It might be time now to take a look at the cabin.",
-        "not_in_game_state": ["scene_investigation/cabin"],
+        "disallowed_story_states": ["scene_investigation/cabin"],
     },
     # not talked about Patrick
     {
         "text": "I think it's time to talk about Patrick. There is stuff you don't know about him yet!",
-        "not_in_game_state": ["character_information/Patrick"],
+        "disallowed_story_states": ["character_information/Patrick"],
     },
     # not talked about Kira
     {
         "text": "We cannot get around talking about Kira. Although I don't want her to be a part of this.",
-        "not_in_game_state": ["character_information/Kira"],
+        "disallowed_story_states": ["character_information/Kira"],
     },
     # body not investigated
     {
         "text": "This is so tough but we need to take a look at the body. I know it's hard but it might help us.",
-        "not_in_game_state": ["scene_investigation/body"],
+        "disallowed_story_states": ["scene_investigation/body"],
     },
     # note not investigated
     {
         "text": "I think we should take a look and read the note. It might help us.",
-        "not_in_game_state": ["scene_investigation/note"],
+        "disallowed_story_states": ["scene_investigation/note"],
     },
     # weapon not investigated
     {
         "text": "I could take a closer look at the weapon.",
-        "not_in_game_state": ["scene_investigation/weapon"],
+        "disallowed_story_states": ["scene_investigation/weapon"],
     },
     # knife not investigated
     {
         "text": "Let's look at the knife, should we?",
-        "not_in_game_state": ["scene_investigation/knife"],
+        "disallowed_story_states": ["scene_investigation/knife"],
     },
     # not asked about full names of characters (because of intitials)
     {
         "text": "I'm not sure you have the full names of the characters. I think you need them if you want to know who could be 'A.P.'",
-        "not_in_game_state": [
+        "disallowed_story_states": [
             "character_information/Maria/full_name",
             "character_information/Anna/full_name",
             "character_information/Patrick/full_name",
@@ -76,7 +76,7 @@ HINTS = [
     # not asked about possible motives of characters
     {
         "text": "Maybe we can think about possible motives of the characters.",
-        "not_in_game_state": [
+        "disallowed_story_states": [
             "motive/Maria",
             "motive/Anna",
             "motive/Patrick",
@@ -87,7 +87,7 @@ HINTS = [
     # not asked about possible access to rollercoaster
     {
         "text": "I think it might help us to clear our minds about who even had access to this rollercoaster and part of the amusement park.",
-        "not_in_game_state": [
+        "disallowed_story_states": [
             "access/Maria",
             "access/Anna",
             "access/Patrick",
@@ -98,7 +98,7 @@ HINTS = [
     # not asked about patricks secret
     {
         "text": "I'm not sure I told you everything about Patrick. There is more. He has a secret...",
-        "not_in_game_state": ["character_information/Patrick/secret"],
+        "disallowed_story_states": ["character_information/Patrick/secret"],
     },
 ]
 
@@ -114,7 +114,7 @@ class Hint(Action):
 
         for hint in HINTS:
             in_game_state = []
-            for state in hint["not_in_game_state"]:
+            for state in hint["disallowed_story_states"]:
                 in_game_state.append(True)
                 keys = state.split("/")
                 temp_data = data["story_state"]
