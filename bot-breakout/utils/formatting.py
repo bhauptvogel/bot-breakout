95f9ce21cc64eea39c648459988fc9d90baae7ec
bhauptvogel <b.vah317@gmail.com>
2023-06-27 12:23:28 +0200

    font family and last names formatted

--- bot-breakout/utils/formatting.py
+++ bot-breakout/utils/formatting.py
@@ -3,16 +3,25 @@ from utils.game_parameters import GameArgs
 
 formattings = {
     "Maria": "<b style='color: FireBrick;'>Maria</b>",
+    "Sterling": "<b><em style='color: FireBrick;'>Sterling</em></b>",
     "Kira": "<b style='color: Navy;'>Kira</b>",
+    "Russell": "<b><em style='color: Navy;'>Russell</em></b>",
     "Victor": "<b style='color: RosyBrown;'>Victor</b>",
+    "Lopez": "<b><em style='color: RosyBrown;'>Lopez</em></b>",
     "Anna": "<b style='color: DarkOrange;'>Anna</b>",
+    "Pollock": "<b><em style='color: DarkOrange;'>Pollock</em></b>",
     "Patrick": "<b style='color: Olive;'>Patrick</b>",
+    "Anyang": "<b><em style='color: Olive;'>Anyang</em></b>",
     "Mika": "<b style='color: DarkSlateGray;'>Mika</b>",
 }
+
+font_family = "font-family: trebuchet ms;"
 
 def utter(dispatcher: CollectingDispatcher, text: str):
     if GameArgs.formatting:
         for key, value in formattings.items():
             text = text.replace(key, value)
         
+        text = f"<p style='{font_family}'>{text}</p>"
+
     dispatcher.utter_message(text=text)
