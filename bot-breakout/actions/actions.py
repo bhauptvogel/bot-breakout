a7ab9e32f096530b0fdfa5872ab6a804eb714380
stephanie <sd9721.sd@gmail.com>
2023-07-02 18:53:16 +0200

    first working version of fallback

--- bot-breakout/actions/actions.py
+++ bot-breakout/actions/actions.py
@@ -185,3 +185,12 @@ class WhoIsTheMurderer(Action):
             utter(dispatcher,text=set_timer(data))
 
         return []
+
+class FallbackAction(Action):
+    def name(self) -> Text:
+        return "action_default_fallback"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        # Custom logic for fallback action
+        dispatcher.utter_message(text="I'm sorry, I didn't understand. Can you please rephrase?")
+        return []
