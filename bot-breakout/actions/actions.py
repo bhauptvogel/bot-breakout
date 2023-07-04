cd9c07d7f4d025b3a70acbebf7555c7036f60156
mickya99 <meikeander99@gmail.com>
2023-07-04 14:02:23 +0200

    smily adjustments

--- bot-breakout/actions/actions.py
+++ bot-breakout/actions/actions.py
@@ -65,7 +65,7 @@ class StartGame(Action):
                 data['timercount'] = 1
         else:
             utter(dispatcher,
-                "This whole situation is really <em>aweful</em> for a date, but I think we are doing good!🥹 Let’s solve this mystery!☺️ We could talk about my coworkers or investigate the room.🔍"
+                "This whole situation is really <em>aweful</em> for a date, but I think we are doing good!🥹 Let’s solve this mystery!😊 We could talk about my coworkers or investigate the room.🔍"
             )
 
         data["first_message_sent"] = True
