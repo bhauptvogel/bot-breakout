2b13e17809c1f1e23d2581611561655c06701180
Ben Niclas Hauptvogel <b.hauptvogel@tu-berlin.de>
2023-07-10 21:03:21 +0000

    Update actions.py

--- bot-breakout/actions/actions.py
+++ bot-breakout/actions/actions.py
@@ -66,7 +66,7 @@ class StartGame(Action):
                 data['timercount'] = 1
         else:
             utter(dispatcher,
-                "This whole situation is really <em>aweful</em> for a date, but I think we are doing good!🥹 Let’s solve this mystery!😊 Besides talking about my coworkers, we can also discuss their relationships or talk about who could have had access to the rollercoaster 🔍"
+                "This whole situation is really <em>aweful</em> for a date, but I think we are doing good!🥹 Let’s solve this mystery!😊 Besides talking about my coworkers, we can also discuss their relations or talk about who could have had access to the rollercoaster 🔍"
             )
 
         data["first_message_sent"] = True
